8d32f866aeecd02a7979fa82b035d768713d1952
jkaralonso <alonsojc622@gmail.com>
2017-06-08 13:41:48 -0500

    Joto niggus soto

--- lunaSOFT/FrontEnd/frmRegistrarPedido.cs
+++ lunaSOFT/FrontEnd/frmRegistrarPedido.cs
@@ -16,5 +16,10 @@ namespace lunaSOFT.FrontEnd
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Vale verga");
+        }
     }
 }
