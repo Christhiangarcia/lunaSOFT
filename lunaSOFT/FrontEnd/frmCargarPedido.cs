2888123cf6d5222a8739dfad8fc2f9ec9ca965c9
Arturo Soto <s13120097@alumnos.itsur.edu.mx>
2017-06-08 13:42:10 -0500

    soto2

--- lunaSOFT/FrontEnd/frmCargarPedido.cs
+++ lunaSOFT/FrontEnd/frmCargarPedido.cs
@@ -21,5 +21,10 @@ namespace lunaSOFT.FrontEnd
         {
 
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Que Puto Eres!!!");
+        }
     }
 }
